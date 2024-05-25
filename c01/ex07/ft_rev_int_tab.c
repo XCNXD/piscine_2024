8ac82b2b64667f8a3e7a63d699417b522d1e69b1
Pattarapol Boonrod <paboonro@z2t2c4.42bangkok.com>
2024-05-25 13:12:58 +0700

    x

---
+++ c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paboonro <paboonro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/25 10:19:13 by paboonro          #+#    #+#             */
+/*   Updated: 2024/05/25 13:09:17 by paboonro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	n_size; 
+	int tmp;
+
+	n_size = 0;
+	while (n_size <= size/2)
+	{
+		tmp = tab[n_size];
+		tab[n_size] = tab[size - 1 - n_size];
+		tab[size - 1 - n_size] = tmp;
+		n_size++;
+	}
+}
