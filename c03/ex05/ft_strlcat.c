773fe33593810d94664ec0bf04ddd80738c9dc83
Natsuiro_XCN <natenanoon@gmail.com>
2024-05-30 02:58:55 +0700

    x

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -1,5 +1,33 @@
+unsigned int	len(char *c)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (c[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	
-	return (dest);
+	unsigned int	dlen;
+	unsigned int	slen;
+	unsigned int	i;
+
+	i = 0;
+	dlen = len(dest);
+	slen = len(src);
+	if (size == 0 || size <= dlen)
+	{
+		return (slen + size);
+	}
+	while (i < size - dlen - 1 && src[i] != 0)
+	{
+		dest[dlen + i] = src[i];
+		i++;
+	}
+	dest[dlen + i] = 0;
+	return (slen + dlen);
 }
