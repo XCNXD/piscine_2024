a170c6828c8f7758d96c02cb7ef54f58a46c2e2f
natsuiro_XCN <natsuiro_XCN@gmail.com>
2024-05-31 20:02:33 +0700

    x

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -10,4 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putnbr_base(int nbr, char *base);
+void	printbase(int *nb, char *base, int len_base)
+{
+	int number;
+	int i;
+	char s[16];
+
+	i = len_base;
+	number = 0;
+	while(i >= 0)
+	{
+		s[i]
+	}
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	carry[16];
+	int base_count;
+	int i = 0;
+
+	base_count = 0;
+	i = 0;
+	while (base[base_count] != 0)
+		base_count++;
+	while (carry/base_count == 0)
+	{
+		carry[i] = carry % base;
+		carry /= base;
+		i++;
+	}
+	carry[i] = carry;
+	printbase(nbr, base, i);
+}
