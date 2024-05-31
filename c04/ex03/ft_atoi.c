034d2cd7cbf116c67a31507988f313e045e8f946
natsuiro_XCN <natsuiro_XCN@gmail.com>
2024-05-31 18:20:14 +0700

    x

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -10,4 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(char *str)
+int	ft_atoi(char *str){
+	int	i;
+	int num;
+	int sign;
+
+	sign = 1;
+	num = 0;
+	i = 0;
+	while (str[i] <= ' ')
+	{
+		i++;
+	}
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num *= 10;
+		num += str[i] - 48;
+		i++;
+	}
+	return (num * sign);
+}
+
+// #include <stdio.h>
+// int main(int argc, char **argv){
+// 	printf("%d %d",argc, ft_atoi(argv[1]));
+// 	return 0;
+// }
