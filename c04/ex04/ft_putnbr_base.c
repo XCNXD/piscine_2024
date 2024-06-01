3af5b83e16c3516beeb74c3877fb65dab31f4389
Natsuiro_XCN <natenanoon@gmail.com>
2024-06-01 17:13:26 +0700

    x

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -10,36 +10,102 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	print_str(char *str, int len)
+{
+	int	i;
+
+	i = len;
+	while (i + 1 != 0)
+	{
+		write(1, str + i, 1);
+		i--;
+	}
+}
+
 void	printbase(int *nb, char *base, int len_base)
 {
-	int number;
-	int i;
-	char s[16];
+	int		i;
+	int		j;
+	char	s[32];
 
 	i = len_base;
-	number = 0;
-	while(i >= 0)
+	j = 0;
+	while (i >= 0)
 	{
-		s[i]
+		s[j] = base[nb[i]];
+		i--;
+		j++;
 	}
+	print_str(s, len_base);
+}
+
+int	len(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+	return (i);
+}
+
+int	check_base_error(char *base)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	k = len(base);
+	if (base[0] == 0 || base[1] == 0)
+		return (0);
+	while (base[i] != 0)
+	{
+		if (base[i] <= 32 || base[i] == 127 || base[i] == 43 || base[i] == 45)
+			return (0);
+		j = i + 1;
+		while (j < k)
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int	carry[16];
-	int base_count;
-	int i = 0;
+	int	carry[32];
+	int	base_count;
+	int	i;
 
 	base_count = 0;
 	i = 0;
-	while (base[base_count] != 0)
-		base_count++;
-	while (carry/base_count == 0)
+	if (check_base_error(base) == 1)
 	{
-		carry[i] = carry % base;
-		carry /= base;
-		i++;
+		if (nbr < 0)
+		{
+			write(1, "-", 1);
+			nbr *= -1;
+		}
+		while (base[base_count] != 0)
+			base_count++;
+		while (nbr / base_count != 0)
+		{
+			carry[i] = nbr % base_count;
+			nbr /= base_count;
+			i++;
+		}
+		carry[i] = nbr;
+		printbase(carry, base, i);
 	}
-	carry[i] = carry;
-	printbase(nbr, base, i);
 }
+
+// int main(){
+// 	ft_putnbr_base(-65535, "0123456789abcdef");
+// }
