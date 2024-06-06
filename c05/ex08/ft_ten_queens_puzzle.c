8059866aab0fd17fb05930dafcd741cbea0b333b
Pattarapol Boonrod <paboonro@z2t4c4.42bangkok.com>
2024-06-06 15:45:21 +0700

    x

--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -1,15 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paboonro <paboonro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 13:47:48 by paboonro          #+#    #+#             */
+/*   Updated: 2024/06/06 15:45:02 by paboonro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int	find_in_array(int *a, int *b, int *c, int i, int j)
+int	find_in_array(int *a, int *b, int *c, int *i_j)
 {
 	int	k;
-	
+	int	i;
+	int	j;
+
 	k = 0;
-	i += 42;
-	j += 42;
+	i = i_j[0] + 42;
+	j = i_j[1] + 42;
 	while (k < 10)
 	{
-		if (a[k] == j || b[k] == i + j || c[k] == i - j)
+		if (a[k] == j || b[k] == i + j || c[k] == i - j + 42)
 		{
 			return (0);
 		}
@@ -18,62 +32,69 @@ int	find_in_array(int *a, int *b, int *c, int i, int j)
 	return (1);
 }
 
-int	array_clear(int *a, int *b, int *c, int i)
+void	array_clear(int *a, int *b, int *c, int i)
 {
 	a[i] = 0;
 	b[i] = 0;
 	c[i] = 0;
 }
 
-int printr(int *row)
+void	printr(int *row, int *count)
 {
-	int	i;
-	char s;
+	int		i;
+	char	s;
 
 	i = 0;
-	while(i++ < 10)
+	*count += 1;
+	while (i < 10)
 	{
-		s = row[i] + 48;
+		s = row[i] + '0';
 		write(1, &s, 1);
+		i++;
 	}
 	write(1, "\n", 1);
 }
 
-void	RecusivE(int i, int *row, int *a, int *b, int *c)
+void	recusive(int i, int *row, int **pointer, int *count)
 {
 	int	j;
+	int	i_j[2];
 
 	j = 0;
 	if (i < 10)
 	{
-		while (j++ < 10)
+		while (j < 10)
 		{
-			if (find_in_array(a, b, c, i, j))
+			i_j[0] = i;
+			i_j[1] = j;
+			if (find_in_array(pointer[0], pointer[1], pointer[2], i_j))
 			{
-				printf("%d %d\n", i, j);
 				row[i] = j;
-				a[i] = j + 42;
-				b[i] = i + j + 84;
-				c[i] = i - j + 84;
-				RecusivE(i + 1, row, a, b, c);
-				array_clear(a, b, c, i);
+				pointer[0][i] = j + 42;
+				pointer[1][i] = i + j + 84;
+				pointer[2][i] = i - j + 42;
+				recusive(i + 1, row, pointer, count);
+				array_clear(pointer[0], pointer[1], pointer[2], i);
 			}
+			j++;
 		}
 	}
 	else
-		printr(row);
+		printr(row, count);
 }
 
-int ft_ten_queens_puzzle(void)
+int	ft_ten_queens_puzzle(void)
 {
 	int	cross_right[10];
 	int	cross_left[10];
-	int	usage_column[10];
-	int usage_row[10];
+	int	usage_column[11];
+	int	usage_row[10];
+	int	*pointer[4];
 
-	RecusivE(0, usage_row, usage_column, cross_right, cross_left);
-}
-
-int main(){
-	ft_ten_queens_puzzle();
+	pointer[0] = usage_column;
+	pointer[1] = cross_right;
+	pointer[2] = cross_left;
+	pointer[3] = &usage_column[11];
+	recusive(0, usage_row, pointer, pointer[3]);
+	return (*pointer[3]);
 }
